3f0d9f791f2b0e8d0bec2a20f667f5d5487e5a61
DESKTOP-HBTG42H\UNCLE CHARLES <appiahmakuta70@gmail.com>
2018-11-30 09:58:53 +0000

    ..

--- public/custom-assets/js/custom.js
+++ public/custom-assets/js/custom.js
@@ -18,12 +18,12 @@ var date_rangepicker_options = {
 };
 
 $(function () {
-    $('.content-wrapper').css('margin-top', $('.navbar-fixed').height() + '3px');
+    $('.content-wrapper').css('margin-top', $('.navbar-fixed').height() + 'px');
     URL_ROOT = $('#url_root').val();
     moment.modifyHolidays.add('Ghana');
 
     $(window).resize(function () {
-        $('.content-wrapper').css('margin-top', $('.navbar-fixed').height() + '3px');
+        $('.content-wrapper').css('margin-top', $('.navbar-fixed').height() + 'px');
     });
 
     //=============================================================
