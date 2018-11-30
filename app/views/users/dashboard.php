de27b20fda1156f943f1628902d9f49328dcfcbe
Eric Clinton Appiah-Kubi <ecakubi@arlgh.com>
2018-11-30 15:26:43 +0000

    Added Previous CMS Forms to Dashboard

--- app/views/users/dashboard.php
+++ app/views/users/dashboard.php
@@ -81,6 +81,93 @@
                                             </a>
                                     </li>
                                     <!-- /.item -->
+                                    <li class="item">
+                                            <a href="#">
+                                                <div class="product-img">
+                                                        <img class="d-none" src="../public/assets/images/adamus.jpg" alt="Product Image" />
+                                                </div>
+                                                <div class="product-info">
+                                                        <span class="product-title text-dark w3-hover-text-dark-blue">
+                                                            Orginator: Eric Akoto (IT Admin) @ IT
+                                                            <br />
+                                                            Date : 12/12/2018
+                                                            <br />
+                                                            Ref: NGM/MW/2.01F1
+                                                        </span>
+                                                        <span class="product-description">
+                                                            Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                                        </span>
+                                                </div>
+                                            </a>
+                                    </li>
+                                    <!-- /.item -->
+                                </ul>
+                            </div>
+                            <!-- /.box-body -->
+                            <div class="box-footer text-center" style="">
+                                <a href="javascript:void(0)" class="uppercase">View All</a>
+                            </div>
+                            <!-- /.box-footer -->
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="box box-primary">
+                            <div class="box-header with-border">
+                                <h3 class="box-title">All Previous CMS Forms <span class="text-muted">(Closed)</span></h3>
+
+                                <div class="box-tools pull-right">
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                                        <i class="fa fa-minus"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-box-tool d-none" data-widget="remove">
+                                        <i class="fa fa-times"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <!-- /.box-header -->
+                            <div class="box-body" style="">
+                                <ul class="products-list product-list-in-box">
+                                    <li class="item">
+                                            <a href="#">
+                                                <div class="product-img">
+                                                        <img class="d-none" src="../public/assets/images/adamus.jpg" alt="Product Image" />
+                                                </div>
+                                                <div class="product-info">
+                                                        <span class="product-title text-dark w3-hover-text-dark-blue">
+                                                            Orginator: Eric Akoto (IT Admin) @ IT
+                                                            <br />
+                                                            Date : 12/12/2018
+                                                            <br />
+                                                            Ref: NGM/MW/2.01F1
+                                                        </span>
+                                                        <span class="product-description">
+                                                            Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                                        </span>
+                                                </div>
+                                            </a>
+                                    </li>
+                                    <!-- /.item -->
+                                    <li class="item">
+                                            <a href="#">
+                                                <div class="product-img">
+                                                        <img class="d-none" src="../public/assets/images/adamus.jpg" alt="Product Image" />
+                                                </div>
+                                                <div class="product-info">
+                                                        <span class="product-title text-dark w3-hover-text-dark-blue">
+                                                            Orginator: Eric Akoto (IT Admin) @ IT
+                                                            <br />
+                                                            Date : 12/12/2018
+                                                            <br />
+                                                            Ref: NGM/MW/2.01F1
+                                                        </span>
+                                                        <span class="product-description">
+                                                            Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                                        </span>
+                                                </div>
+                                            </a>
+                                    </li>
+                                    <!-- /.item -->
                                 </ul>
                             </div>
                             <!-- /.box-body -->
